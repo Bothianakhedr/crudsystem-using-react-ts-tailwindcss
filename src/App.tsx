7e6a5eb56911c07abd3acd377d243ed5e59fb08f
Bothianakhedr <Bothianakhedr198@gmail.com>
2025-06-28 14:18:23 +0300

    enhance product card style

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <main className="my-4">
 
-      <div className="container mx-auto  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="container mx-auto gap-4  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 {productInfo.map((product)=>{
   return  <ProductCard product={product} key={product.id} />
 })}
--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -3,19 +3,19 @@ import ReusableImage from "../ReusableImage/ReusableImage";
 import Button from "../ui/Button";
 
 interface IProps {
-  product:IProduct
+  product: IProduct;
 }
 
-const ProductCard = ({product}: IProps) => {
-  const{title,description,imageURL,price,id,category}=product
+const ProductCard = ({ product }: IProps) => {
+  const { title, description, imageURL, price, id, category } = product;
   return (
-    <div className="border border-gray-200 rounded p-3 ">
+    <div className="border border-gray-200 rounded p-3 m-2 md:m-0">
       <ReusableImage
-        className="rounded object-cover h-55 w-full mb-2"
+        className="rounded object-cover h-53 w-full mb-2"
         alt={title}
         imageURL={imageURL}
       />
-      <h3 className="text-[16px]">{title}</h3>
+      <h3 className="text-[16px] font-semibold">{title}</h3>
       <p className="my-1 text-[14px] line-clamp-3 text-slate-700">
         {description}
       </p>
@@ -30,15 +30,26 @@ const ProductCard = ({product}: IProps) => {
         <span>{price}</span>
 
         <ReusableImage
-          className="w-5 h-5 rounded-full object-bottom"
-          alt={title}
-          imageURL={imageURL}
+          className="w-7 h-7 rounded-full object-bottom"
+          alt={category.name}
+          imageURL={category.imageURL}
         />
       </div>
 
       <div className="flex items-center justify-center gap-2 ">
-        <Button width="w-full" onClick={()=>{alert("edited")}} className="bg-blue-700  ">EDIT</Button>
-        <Button  width="w-full" className="bg-red-900 "> DESTROY</Button>
+        <Button
+          width="w-full"
+          onClick={() => {
+            alert("edited");
+          }}
+          className="bg-blue-700  "
+        >
+          EDIT
+        </Button>
+        <Button width="w-full" className="bg-red-800 ">
+          {" "}
+          DESTROY
+        </Button>
       </div>
     </div>
   );
